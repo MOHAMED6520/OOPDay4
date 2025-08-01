286481795d85a18286e2dba5fe0dbe30afee0f68
moham <moham@DESKTOP-M8CT1H1>
2025-08-02 00:11:06 +0300

    Project 1 Q01 && Q02

---
+++ OOPDay4/First Project/Duration.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDay4.First_Project
+{
+    internal class Duration
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        private int _hours = 0 , _minutes = 0  , _seconds =0 ;
+        public Duration(int hours , int minutes , int seconds)
+        {
+            if (seconds >= 60)
+            {
+                _minutes= seconds /60;
+                seconds = seconds %60;
+                
+            }
+            Seconds = seconds;
+            minutes = minutes + _minutes;
+
+            if (minutes >= 60)
+            {
+                _hours= minutes /60;
+                minutes = minutes % 60;
+                
+            }
+            Minutes = minutes;
+            Hours = hours+_hours;
+        }
+
+        public Duration (int seconds)
+        {
+            if (seconds >= 60)
+            {
+                _minutes=seconds /60;
+                seconds = seconds % 60;
+                
+            }
+            Seconds = seconds;
+
+            if (_minutes >=60)
+            {
+                _hours = _minutes / 60;
+                _minutes = _minutes % 60;
+                
+            }
+            Minutes=_minutes;
+
+            Hours=_hours;
+        }
+      
+        public Duration()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            if (Hours == 0)
+                {
+                if (Minutes == 0)
+                    return $"Seconds : {Seconds}";
+                else
+                    return $"Minutes : {Minutes}, Seconds : {Seconds}";
+            }
+            else
+                return $"Hours: {Hours} , Minutes : {Minutes}, Seconds : {Seconds}";
+        }
+    }
+}
--- OOPDay4/Program.cs
+++ OOPDay4/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace OOPDay4
+﻿using Microsoft.VisualBasic;
+using OOPDay4.First_Project;
+
+namespace OOPDay4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Project 1 Q01 && Q02
+
+            #region Project 1 Q01 && Q02
+            //Duration D0 = new Duration(1, 10, 15);
+            //Console.WriteLine(D0.ToString());
+
+            //Console.WriteLine();
+
+            //Duration D1 = new Duration(3600);
+            //Console.WriteLine(D1.ToString());
+
+            //Console.WriteLine();
+
+            //Duration D2 = new Duration(7800);
+            //Console.WriteLine(D2.ToString());
+
+            //Console.WriteLine();
+
+            //Duration D3 = new Duration(666);
+            //Console.WriteLine(D3.ToString()); 
+            #endregion
+
         }
     }
 }
