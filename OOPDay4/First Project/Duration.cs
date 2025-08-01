86ba13c452cf25f2cef8df82428f1b871ed88111
moham <moham@DESKTOP-M8CT1H1>
2025-08-02 00:42:52 +0300

    Project 1 Q01 && Q02

--- OOPDay4/First Project/Duration.cs
+++ OOPDay4/First Project/Duration.cs
@@ -13,64 +13,50 @@ namespace OOPDay4.First_Project
         public int Seconds { get; set; }
 
         private int _hours = 0 , _minutes = 0  , _seconds =0 ;
-        public Duration(int hours , int minutes , int seconds)
+        public Duration(int hours, int minutes, int seconds) : this(seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+
+        }
+
+        public Duration(int seconds)
+        {
+            Seconds = seconds;
+        }
+        public Duration()
+        {
+
+        }
+        private string CalcDUration(int hours, int minutes, int seconds)
         {
             if (seconds >= 60)
             {
-                _minutes= seconds /60;
-                seconds = seconds %60;
-                
+                _minutes = seconds / 60;
+                seconds = seconds % 60;
+
             }
-            Seconds = seconds;
+          
             minutes = minutes + _minutes;
 
             if (minutes >= 60)
             {
-                _hours= minutes /60;
+                _hours = minutes / 60;
                 minutes = minutes % 60;
-                
+
             }
             Minutes = minutes;
-            Hours = hours+_hours;
+           
+            hours = hours + _hours;
+
+            return $"Hours: {hours} , Minutes : {minutes}, Seconds : {seconds}";
         }
-
-        public Duration (int seconds)
-        {
-            if (seconds >= 60)
-            {
-                _minutes=seconds /60;
-                seconds = seconds % 60;
-                
-            }
-            Seconds = seconds;
-
-            if (_minutes >=60)
-            {
-                _hours = _minutes / 60;
-                _minutes = _minutes % 60;
-                
-            }
-            Minutes=_minutes;
-
-            Hours=_hours;
-        }
+ 
       
-        public Duration()
-        {
-            
-        }
 
         public override string ToString()
         {
-            if (Hours == 0)
-                {
-                if (Minutes == 0)
-                    return $"Seconds : {Seconds}";
-                else
-                    return $"Minutes : {Minutes}, Seconds : {Seconds}";
-            }
-            else
-                return $"Hours: {Hours} , Minutes : {Minutes}, Seconds : {Seconds}";
+            return CalcDUration (Hours, Minutes, Seconds);
         }
     }
 }
--- OOPDay4/Program.cs
+++ OOPDay4/Program.cs
@@ -7,27 +7,28 @@ namespace OOPDay4
     {
         static void Main(string[] args)
         {
-            //Project 1 Q01 && Q02
 
             #region Project 1 Q01 && Q02
-            //Duration D0 = new Duration(1, 10, 15);
-            //Console.WriteLine(D0.ToString());
+            Duration D0 = new Duration(1, 10, 15);
+            Console.WriteLine(D0.ToString());
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Duration D1 = new Duration(3600);
-            //Console.WriteLine(D1.ToString());
+            Duration D1 = new Duration(3600);
+            Console.WriteLine(D1.ToString());
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Duration D2 = new Duration(7800);
-            //Console.WriteLine(D2.ToString());
+            Duration D2 = new Duration(7800);
+            Console.WriteLine(D2.ToString());
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Duration D3 = new Duration(666);
-            //Console.WriteLine(D3.ToString()); 
+            Duration D3 = new Duration(666);
+            Console.WriteLine(D3.ToString());
             #endregion
+
+
 
         }
     }
