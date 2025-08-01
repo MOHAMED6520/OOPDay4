91e87de87690fd6892aab4890043161ab58a11f8
moham <moham@DESKTOP-M8CT1H1>
2025-08-01 15:50:31 +0300

    Abstraction

---
+++ OOPDay4/Acstraction/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDay4.Acstraction
+{
+    internal class Rectangle : Shape
+    {
+        public override double perimeter
+        {
+            get
+            { 
+                return (Dim01 + Dim02)*2;
+            } 
+        }
+
+        public override double GetArea()
+        {
+            return Dim01 * Dim02;   
+        }
+
+
+    }
+}
--- OOPDay4/Program.cs
+++ OOPDay4/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace OOPDay4
+﻿using OOPDay4.Acstraction;
+
+namespace OOPDay4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Abstraction
+            /////*Abstraction*/
+
+            ////Shape shape = new Shape(); //Invalid
+            //Rectangle r = new Rectangle() { Dim01 = 5, Dim02 = 3 };
+            //Console.WriteLine(r.perimeter);
+            //Console.WriteLine(r.GetArea());
+
+            //Square square = new Square() { Dim01 = 2, Dim02 = 5 };
+
+            //Console.WriteLine(square.perimeter);
+            //Console.WriteLine(square.GetArea()); 
+            #endregion
         }
     }
 }
