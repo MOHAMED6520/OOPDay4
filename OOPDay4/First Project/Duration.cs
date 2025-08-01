abc83f6642a50a4336be9fd8f4ed4e3277db5b6f
moham <moham@DESKTOP-M8CT1H1>
2025-08-02 01:33:13 +0300

    Project01 Q03

--- OOPDay4/First Project/Duration.cs
+++ OOPDay4/First Project/Duration.cs
@@ -49,10 +49,128 @@ namespace OOPDay4.First_Project
            
             hours = hours + _hours;
 
+            if (seconds <0 )
+               {
+                if (minutes <= 0)
+                {
+                    hours = hours - 1; 
+                    minutes = minutes+59;
+                    seconds = seconds + 60;
+                }
+                else
+                {
+                    minutes = minutes -1;
+                    seconds = seconds + 60;
+                }
+               }
+
+            if (minutes < 0)
+            {
+                hours = hours - 1;
+                minutes = minutes + 60;
+                
+            }
+
+            if (hours < 0 )
+             {
+                hours = 0; 
+                minutes = 0;
+                seconds = 0;
+             }
+                
+
             return $"Hours: {hours} , Minutes : {minutes}, Seconds : {seconds}";
         }
- 
-      
+
+        public static Duration operator +(Duration a, Duration b)
+        {
+            return new Duration()
+            {
+                Hours = a.Hours + b.Hours,
+                Minutes = a.Minutes + b.Minutes,
+                Seconds = a.Seconds + b.Seconds
+            };
+        }
+
+        public static Duration operator +(Duration a, int b)
+        {
+            return new Duration()
+            {
+              Seconds=a.Seconds + b,
+              Minutes=a.Minutes ,
+              Hours = a.Hours ,
+              
+            };
+        }
+
+        public static Duration operator +(int b, Duration a)
+        {
+            return new Duration()
+            {
+                Seconds = a.Seconds + b,
+                Minutes = a.Minutes,
+                Hours = a.Hours,
+            };
+        }
+
+        public static Duration operator ++(Duration D)
+        {
+            D.Minutes++;
+            return new Duration () 
+            {
+                Seconds = D.Seconds ,
+                Minutes = D.Minutes,
+                Hours =   D.Hours,
+            };
+        }
+
+        public static Duration operator --(Duration D)
+        {
+            D.Minutes--;
+            return new Duration()
+            {
+                Seconds = D.Seconds,
+                Minutes = D.Minutes,
+                Hours = D.Hours,
+            };
+        }
+
+        public static Duration operator -(Duration a, Duration b)
+        {
+            return new Duration()
+            {
+                Hours = a.Hours - b.Hours,
+                Minutes = a.Minutes - b.Minutes,
+                Seconds = a.Seconds - b.Seconds
+            };
+        }
+
+        public static bool operator >(Duration left , Duration right)
+        {
+            if (left.Hours == right.Hours)
+            {
+                if (left.Minutes == right.Minutes)
+                    return left.Seconds > right.Seconds;
+                else
+                    return left.Minutes > right.Minutes;
+            }
+            else 
+                return left.Hours > right.Hours;
+        }
+
+        public static bool operator <(Duration left, Duration right)
+        {
+            if (left.Hours == right.Hours)
+            {
+                if (left.Minutes == right.Minutes)
+                    return left.Seconds < right.Seconds;
+                else
+                    return left.Minutes < right.Minutes;
+            }
+            else
+                return left.Hours < right.Hours;
+        }
+
 
         public override string ToString()
         {
--- OOPDay4/Program.cs
+++ OOPDay4/Program.cs
@@ -9,25 +9,56 @@ namespace OOPDay4
         {
 
             #region Project 1 Q01 && Q02
-            Duration D0 = new Duration(1, 10, 15);
-            Console.WriteLine(D0.ToString());
+            //Duration D0 = new Duration(1, 10, 15);
+            //Console.WriteLine(D0.ToString());
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Duration D1 = new Duration(3600);
-            Console.WriteLine(D1.ToString());
+            //Duration D1 = new Duration(3600);
+            //Console.WriteLine(D1.ToString());
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Duration D2 = new Duration(7800);
-            Console.WriteLine(D2.ToString());
+            //Duration D2 = new Duration(7800);
+            //Console.WriteLine(D2.ToString());
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Duration D3 = new Duration(666);
-            Console.WriteLine(D3.ToString());
+            //Duration D3 = new Duration(666);
+            //Console.WriteLine(D3.ToString());
             #endregion
 
+
+            
+
+            #region Project01 Q03
+            //Duration D1 = new Duration() { Hours = 5, Minutes = 10 , Seconds =5 };
+            //Duration D2 = new Duration() { Hours = 1, Minutes = 30, Seconds = 40 };
+            //Duration D3;
+
+            //D3 = D1 + D2;
+            //Console.WriteLine(D3);
+
+            //D3 = D1 + 7800;
+            //Console.WriteLine(D3);
+
+            //D3 = 666 + D3;
+            //Console.WriteLine(D3);
+
+            //D3 = ++D1;
+            //Console.WriteLine(D3);
+
+            //D3 = --D2;
+            //Console.WriteLine(D3);
+
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1);
+
+            //if (D1 > D2)
+            //{
+            //    Console.WriteLine("D1 > D2");
+            //} 
+            #endregion
 
 
         }
